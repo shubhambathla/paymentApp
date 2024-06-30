1fa5f90a9ba6db25af0ed9a545508742c3d898e4
shubhambathla <shubhambathla116@gmail.com>
2024-06-30 15:56:58 +0530

    Create middleware for user authentication

---
+++ backend/middleware.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("./config");
+
+const app = express();
+
+const authMiddleware = (req, res, next) => {
+  const tokenLoad = req.headers.authorization;
+  if (!tokenLoad || !tokenLoad.startsWith("Bearer ")) {
+    //Checks the headers for an Authorization header Bearer <token>
+    return res.status(403).json({
+      message: "Invalid Token",
+    });
+  }
+  const token = tokenLoad.split(" ")[1];
+  try {
+    const tokenVerify = jwt.verify(token, JWT_SECRET); //Verifies that the token is valid
+    req.userId = tokenVerify.userId; //Puts the userId in the request object if the token checks out.
+    next();
+  } catch (err) {
+    return res.status(403).json({});
+  }
+};
+
+module.exports = {
+  authMiddleware,
+};
