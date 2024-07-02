067be279a138a1de8fdb6614082695e099d777e4
shubhambathla <shubhambathla116@gmail.com>
2024-07-02 19:36:09 +0530

    Create Signup Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Signin from "./pages/Signin"
+import UserSignup from "./pages/UserSignup"
+import Dashboard from "./pages/Dashboard"
+import SendMoney from "./pages/SendMoney"
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hi there!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<UserSignup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
