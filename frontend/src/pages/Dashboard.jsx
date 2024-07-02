b883deb14295bcc72e228e9a9b234d27eee7db7f
shubhambathla <shubhambathla116@gmail.com>
2024-07-02 21:13:46 +0530

    Create Dashboard page with user filter to send money

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
+import { Balance } from "../components/Balance"
+import  AllUsers  from "../components/AllUsers"
 function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+    return <div>
+        <div className="m-8">
+            <Balance value={"10,000"} />
+            <AllUsers />
+        </div>
+    </div>
 }
 
 export default Dashboard
