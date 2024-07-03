4dbf9c05cfbc1f8ef301baa4d043318390f322c8
shubhambathla <shubhambathla116@gmail.com>
2024-07-03 09:44:57 +0530

    Add Navigation bar

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,9 @@
 import { Balance } from "../components/Balance"
 import  AllUsers  from "../components/AllUsers"
+import Appbar from "../components/Appbar"
 function Dashboard() {
     return <div>
+        <Appbar/>
         <div className="m-8">
             <Balance value={"10,000"} />
             <AllUsers />
